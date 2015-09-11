07776fc31a3988c1a2353bfdfb3a2d04cd0e44c8
Jelly King <curriedmc@gmail.com>
2015-09-11 23:27:37 +0100

    2d Array interaction and padding complete
    2d array interaction and padding completed. Now interacts with 2d array
    and can pass values into other method.

--- VBIntro/Module1.vb
+++ VBIntro/Module1.vb
@@ -3,9 +3,13 @@
     Sub Main()
         Console.WriteLine("Hello")
         writeHello()
+        fill2DArray()
+
 
     End Sub
+    'code stepping: step into = look into function calls. step over= avoid function calls.
 
+    'console.writeline
     Sub writeHello()
         Console.WriteLine("Hello there")
         'call method below
@@ -13,6 +17,7 @@
 
     End Sub
 
+    'array interacton
     Sub fillArray()
         'method which generaes random numbers, fills array, prints array. move to list?
         Dim numberArray(6) As Integer 'this is an array
@@ -31,7 +36,50 @@
         Next
 
     End Sub
-    'now to add in a 2D array
+
+    '2darray interaction
+    Sub fill2DArray()
+        Dim numberArray(10, 10) As Integer
+        Randomize()
+        Dim arrayNumberCount As Integer
+
+
+        For i As Integer = 0 To 9 'change to under 10
+            For j As Integer = 0 To 9
+                numberArray(i, j) = arrayNumberCount
+                arrayNumberCount = arrayNumberCount + 1
+            Next
+        Next
+
+        For i As Integer = 0 To 9
+            For j As Integer = 0 To 9
+                'Console.Write(numberArray(i - 1, j - 1))
+                padding(numberArray(i, j))
+
+            Next
+            Console.WriteLine()
+
+        Next
+
+    End Sub
+
+    'padding method for above 2darray
+    Sub padding(ByVal numberToPad As Integer)
+
+        If numberToPad < 10 Then
+            Console.Write("  ")
+            Console.Write(numberToPad)
+        End If
+        If numberToPad >= 10 And numberToPad < 100 Then
+            Console.Write(" ")
+            Console.Write(numberToPad)
+        End If
+        If numberToPad = 100 Then
+            Console.WriteLine(numberToPad)
+        End If
+
+    End Sub
+
     'now to add in enums
     'now to add in a switch case menu
 
